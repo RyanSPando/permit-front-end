31e2dbc3f0dafdb8450279d0da05fce442cd2780
Ryan "archaen" Pando <vretha@gmail.com>
2016-12-07 12:26:03 -0700

    Server ready for radius check for pin.

--- src/js/splash.js
+++ src/js/splash.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  const url = 'https://permit-node-server.herokuapp.com/data/radius';
   $("#splash-btn").click(function () {
     var input = $('#auto-input').val();
       $("html, body").animate({ scrollTop: $("#navbar").offset().top }, 500);
@@ -6,6 +7,7 @@ $(document).ready(function() {
         $.ajax(`https://maps.googleapis.com/maps/api/geocode/json?address=${input}`)
         .then(pinData => {
           const results = pinData.results[0];
+          console.log(results);
           setTimeout(function () {
             var marker = new google.maps.Marker({
               position: results.geometry.location,
@@ -15,6 +17,11 @@ $(document).ready(function() {
               icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
             });
           }, 1000);
+          const location = results.geometry.location;
+          $.ajax(`${url}?lat=${location.lat}&lon=${location.lng}`)
+          .then(mapData => {
+            console.log(mapData);
+          });
         });
       }
       return true;
