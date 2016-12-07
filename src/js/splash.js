7380bf25cbf555a728dc7470bd9196a29289c1a5
Ryan "archaen" Pando <vretha@gmail.com>
2016-12-07 14:30:17 -0700

    Zillow route route for CORS.

--- src/js/splash.js
+++ src/js/splash.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   const url = 'https://permit-node-server.herokuapp.com/data/radius';
+  const url2 = 'https://permit-node-server.herokuapp.com/data/zillow';
   $("#splash-btn").click(function () {
     var input = $('#auto-input').val();
       $("html, body").animate({ scrollTop: $("#navbar").offset().top }, 500);
@@ -7,7 +8,6 @@ $(document).ready(function() {
         $.ajax(`https://maps.googleapis.com/maps/api/geocode/json?address=${input}`)
         .then(pinData => {
           const results = pinData.results[0];
-          console.log(results);
           setTimeout(function () {
             var marker = new google.maps.Marker({
               position: results.geometry.location,
@@ -16,11 +16,20 @@ $(document).ready(function() {
               animation: google.maps.Animation.DROP,
               icon: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
             });
-          }, 1000);
-          const location = results.geometry.location;
-          $.ajax(`${url}?lat=${location.lat}&lon=${location.lng}`)
-          .then(mapData => {
-            console.log(mapData);
+            map.setCenter(marker.getPosition());
+            map.setZoom(13);
+            const location = results.geometry.location;
+            $.ajax(`${url}?lat=${location.lat}&lon=${location.lng}`)
+            .then(mapData => {
+              const heatLayerData = mapData.rows.map(data => {
+                return new google.maps.LatLng(data.lat, data.lon);
+              });
+              layer_0.setData(heatLayerData);
+            });
+          },10);
+          $.ajax(`${url2}?address=${input}`)
+          .then(zillow => {
+            console.log(zillow);
           });
         });
       }
