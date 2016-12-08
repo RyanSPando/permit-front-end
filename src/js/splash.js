17c7987240bef0dfd790861e7d25ec608f7ea2d2
Ryan "archaen" Pando <vretha@gmail.com>
2016-12-07 22:40:04 -0700

    Working graph on permit data with pin drop.

--- src/js/splash.js
+++ src/js/splash.js
@@ -1,4 +1,6 @@
+
 $(document).ready(function() {
+  google.charts.load('current', {packages: ['corechart', 'bar']});
   const url = 'https://permit-node-server.herokuapp.com/data/radius';
   const url2 = 'https://permit-node-server.herokuapp.com/data/zillow';
   $("#splash-btn").click(function () {
@@ -25,13 +27,60 @@ $(document).ready(function() {
                 return new google.maps.LatLng(data.lat, data.lon);
               });
               layer_0.setData(heatLayerData);
+
+              var data = new google.visualization.DataTable();
+
+              var permitData = [
+                ['January',  0],
+                ['February',  0],
+                ['March',  0],
+                ['April',  0],
+                ['May',  0],
+                ['June',  0],
+                ['July',   0],
+                ['August',  0],
+                ['September',  0],
+                ['October', 0],
+                ['November',  0],
+                ['December',  0]
+              ];
+
+              mapData.rows.forEach(permit => {
+                let index = parseInt(permit.AppliedDate.slice(5, 7));
+                permitData[index - 1][1] += parseInt(permit.EstProjectCost);
+              });
+
+              data.addColumn('string', 'Month');
+              data.addColumn('number', 'Total Permit Value');
+              data.addRows(permitData);
+
+              var options = {
+                  title: 'Total permit value by Month',
+                  annotations: {
+                    alwaysOutside: false,
+                    textStyle: {
+                      fontSize: 10,
+                      color: '#000',
+                      auraColor: 'none'
+                    }
+                  },
+                  hAxis: {
+                    title: 'Month',
+                    textPosition: 'none'
+                  },
+                  vAxis: {
+                    title: 'Total Permi Value USD'
+                  },
+                  legend: {
+                    position: 'none'
+                  }
+                };
+
+              var chart = new google.visualization.ColumnChart(document.getElementById('chart'));
+              chart.draw(data, options);
             });
-          },10);
-          $.ajax(`${url2}?address=${input}`)
-          .then(zillow => {
-            console.log(zillow);
           });
-        });
+        },1000);
       }
       return true;
   });
